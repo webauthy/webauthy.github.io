9ef3aeebb4eb9472db63a55a2c42a7615bdbd31b
Token Yi <token@flashbay.com>
2023-08-09 18:33:46 +0800

    Add service worker to support running offline

---
+++ js/app.js
@@ -0,0 +1,184 @@
+var UIInterface = {
+    _callFuncCatchError: function (func){
+        try {
+            if (isFunction(func)) {
+                return func.call(this);
+            }
+        } catch (e) {
+            console.log(e);
+            alert(JSON.stringify(e));
+        }
+    },
+    addByImage: function (callback){
+        this._callFuncCatchError(function () {
+            var _self = this;
+            decodeLocalImage(function (content){
+                onDecodeComplete(content, callback);
+            });
+        });
+    },
+    removeItem: function (id){
+        this._callFuncCatchError(function(){
+            if (confirm('Confirm to remove it?')) {
+                ItemsManager.remove(id);
+            }
+        });
+    },
+    backup: function (){
+        this._callFuncCatchError(function(){
+            var encryptNeeded = false, fileName = formatDate('TOTP_Authenticator_backup_%Y%m%d%H%M%S.totp');
+            exportToFile(encryptNeeded, fileName);
+        });
+    },
+    restore: function (){
+        this._callFuncCatchError(function(){
+            var _self = this;
+            var overwrite = false;
+            importFromFile({overwrite:overwrite, onRestored: function(skippedItems){
+                //console.log(skippedItems);
+            }});
+        });
+    }, 
+    refresh: function (){
+        this._callFuncCatchError(function(){
+            this.renderOtpList();
+        });
+    },
+    donate: function (){
+        if ($Id('donate').style.display == 'block') {
+            $Id('donate').style.display = 'none';
+        } else {
+            $Id('donate').style.display = 'block';
+        }
+    }
+}
+
+function removeItem(id){
+    UIInterface.removeItem(id);
+}
+
+function onDecodeComplete(content, callback){
+    isFunction(callback) && callback(content);
+    if (!testOtpAuthUri(content)) return;
+    var uiitem = handleOtpAuthUrl(content);
+    if (uiitem) uiitem.flashBorder();
+}
+//handleOtpAuthUrl('otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=10');
+function handleOtpAuthUrl(url){
+    try {
+        var OTPEntity = parseOtpAuthUri(url);
+        if (ItemsManager.has(OTPEntity.id)) {
+            ItemsManager.updateSecret(OTPEntity);
+        } else {
+            ItemsManager.showOTPCode(OTPEntity);
+        }
+        return ItemsManager.getUIItem(OTPEntity.id);
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+function exportToFile(encrypting, filename){ //to do
+    filename = filename || 'optauth_accounts.totp';
+    encrypting = encrypting || false;
+    var downloadUrl = ItemsManager.genBackupUrl(encrypting);
+    downloadUrl && forceDownload(downloadUrl, filename);
+}
+
+function importFromFile(options){ //to do
+    options = options || {};
+    openFileDlg(function(content){
+        var skippedItems = ItemsManager.restoreFromBackup(content, options['overwrite'] || false);
+        isFunction(options['onRestored']) && options['onRestored'].apply(null, [skippedItems]);
+    }, {accept: '.totp', mimeType: 'text/totp'});
+}
+
+function showMenu(e){
+    $Id('more-menu').style.display = 'block';
+    e.stopPropagation();
+}
+
+function hideOverlays(){
+    var nodelist = document.querySelectorAll('.overlay');
+    for (var i in nodelist) {
+        if (nodelist.hasOwnProperty(i)) {
+            nodelist[i].style.display = 'none';
+        }
+    }
+}
+
+
+window.onload = function () {
+    $on(document, 'click', function (){
+        hideOverlays();
+    });
+    $on('scan-btn', 'click', function (){
+        Scanner.open();
+    });
+    $on('pickImage-btn', 'click', function (){
+        UIInterface.addByImage(function(content){
+            //console.log(content);
+        });
+    });
+    $on('more-btn', 'click', function (e){
+        showMenu(e);
+    });
+    $on('refresh-btn', 'click', function (){
+        location.reload();
+    });
+    $on('backup-btn', 'click', function (){
+        UIInterface.backup();
+    });
+    $on('restore-btn', 'click', function (){
+        UIInterface.restore();
+    });
+    $on('donate-btn', 'click', function (){
+        UIInterface.donate();
+    });
+    $on('donate-close-btn', 'click', function (){
+        UIInterface.donate();
+    });
+    
+    ItemsManager.setContainer($Id('container')).loadFromStorage(function (items){
+        if (items.length == 0) {
+            // 样例展示
+            var sample = {
+                id: 'totp/sample.com:test@sample.com',
+                schema: 'totp',
+                account: "test@sample.com",
+                secret: "HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ",
+                issuer: "sample.com",
+                algorithm: "SHA1",
+                digits: 6,
+                period: 30
+            };
+            ItemsManager.showOTPCode(sample, false);
+        }
+        
+        setSecondTick(function (){
+            ItemsManager.observe();
+        });
+    });
+    
+    setTimeout(function () {
+        document.getElementById('msg_toast').style.height='0px';
+    }, 6000);
+}
+
+function swRegister(){
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js', { scope: './' }).then(function (reg){
+            if (reg.installing) {
+                console.log('Service worker installing');
+            } else if (reg.waiting) {
+                console.log('Service worker installed');
+            } else if (reg.active) {
+                console.log('Service worker active');
+            }
+        }).catch(function (e){
+            console.log('Registration failed with ' + e);
+        });
+    }
+}
+
+swRegister();
---
+++ sw.js
@@ -0,0 +1,64 @@
+/**
+ * https://developer.mozilla.org/zh-CN/docs/Web/API/Service_Worker_API/Using_Service_Workers
+ */
+ 
+var CACHE_NAME = 'gauthn-cache-v1';
+var INITIAL_RESOURCES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/error.html',
+    '/audio/beep.mp3',
+    '/css/en-us.css',
+    '/css/zh-cn.css',
+    '/images/alipay.svg',
+    '/images/paypal.svg',
+    '/js/ItemsManager.js',
+    '/js/jsQR.min.js',
+    '/js/jsTOTP-es5.min.js',
+    '/js/llqrcode.min.js',
+    '/js/localforage.min.js',
+    '/js/scanner.js',
+    '/js/sjcl.js',
+    '/js/tools.js',
+    '/js/UIItem.js',
+    '/js/app.js'
+];
+
+// On install, fill the cache with the initial resources.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(INITIAL_RESOURCES_TO_CACHE);
+    })());
+});
+
+// On fetch events, do a network-first approach, so we can more easily work on the app for the time being.
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+
+        try {
+            const fetchResponse = await fetch(event.request);
+            if (fetchResponse.status === 200) {
+                // Save the new resource in the cache (responses are streams, so we need to clone in order to use it here).
+                cache.put(event.request, fetchResponse.clone());
+            }
+
+            // And return it.
+            return fetchResponse;
+        } finally {
+            // Fetching didn't work let's go to the cache.
+            const cachedResponse = await cache.match(event.request);
+            if (cachedResponse !== undefined) {
+                // Cache hit, let's send the cached resource.
+                return cachedResponse;
+            } else {
+                // Nothing in cache, let's go to the error page.
+                if (event.request.mode === 'navigate') {
+                    var errorResponse = await cache.match('error.html');
+                    return errorResponse;
+                }
+            }
+        }
+    })());
+});
